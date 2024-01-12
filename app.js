c3ebe7063a45f4b111c549464e76cb9931c3b655
chinmaya-elred <chinmaya@elred.io>
2024-01-12 14:00:03 +0530

    completed till 185 videos from section 12

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+const mongoConnect = require('./util/dabase').mongoConnect;
 
 const app = express();
 
@@ -16,9 +17,23 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  // User.findById(1)
+  //   .then(user => {
+  //     req.user = user;
+  //     next();
+  //   })
+  //   .catch(err => console.log(err));
+  next();
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+
+mongoConnect((client) =>{
+    app.listen(3000);
+    console.log(client, 'app.js file')
+})
